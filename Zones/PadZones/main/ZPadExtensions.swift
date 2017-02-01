29fa498a6fbaf4814d1019297073668d0bd01784
Jonathan Sand <sand@gizmolab.com>
2017-01-31 18:11:59 -0800

    phase two: split extensions apart (core, iOS and OS X)

---
+++ Zones/PadZones/main/ZPadExtensions.swift
@@ -0,0 +1,145 @@
+//
+//  ZPadExtensions.swift
+//  Zones
+//
+//  Created by Jonathan Sand on 10/8/16.
+//  Copyright © 2016 Zones. All rights reserved.
+//
+
+
+import Foundation
+import CloudKit
+import UIKit
+
+
+public typealias ZFont                      = UIFont
+public typealias ZView                      = UIView
+public typealias ZEvent                     = UIEvent
+public typealias ZImage                     = UIImage
+public typealias ZColor                     = UIColor
+public typealias ZButton                    = UIButton
+public typealias ZWindow                    = UIWindow
+public typealias ZSlider                    = UISlider
+public typealias ZControl                   = UIControl
+public typealias ZMenuItem                  = UIMenuItem
+public typealias ZTextView                  = UITextView
+public typealias ZTextField                 = UITextField
+public typealias ZTableView                 = UITableView
+public typealias ZBezierPath                = UIBezierPath
+public typealias ZSearchField               = UISearchBar
+public typealias ZApplication               = UIApplication
+public typealias ZViewController            = UIViewController
+public typealias ZSegmentedControl          = UISegmentedControl
+public typealias ZGestureRecognizer         = UIGestureRecognizer
+public typealias ZTextFieldDelegate         = UITextFieldDelegate
+public typealias ZProgressIndicator         = UIActivityIndicatorView
+public typealias ZTableViewDelegate         = UITableViewDelegate
+public typealias ZSearchFieldDelegate       = UISearchBarDelegate
+public typealias ZTableViewDataSource       = UITableViewDataSource
+public typealias ZApplicationDelegate       = UIApplicationDelegate
+public typealias ZGestureRecognizerDelegate = UIGestureRecognizerDelegate
+
+
+let zapplication = ZApplication.shared
+
+
+func NSStringFromSize(_ size: CGSize) -> String {
+    return NSStringFromCGSize(size)
+}
+
+
+extension UIColor {
+    func darker(by: CGFloat) -> UIColor {
+        var        hue: CGFloat = 0.0
+        var      alpha: CGFloat = 0.0
+        var saturation: CGFloat = 0.0
+        var brightness: CGFloat = 0.0
+
+        self.getHue(&hue, saturation: &saturation, brightness: &brightness, alpha: &alpha)
+
+        return UIColor(hue: hue, saturation: saturation, brightness: brightness, alpha: alpha)
+    }
+}
+
+
+extension UIApplication {
+
+    func presentError(_ error: NSError) {}
+
+
+    func clearBadge() {
+        applicationIconBadgeNumber += 1
+        applicationIconBadgeNumber  = 0
+
+        cancelAllLocalNotifications()
+    }
+}
+
+
+extension UIWindow {
+    @discardableResult func makeFirstResponder(_ responder: UIResponder?) -> Bool {
+        return responder == nil ? false : responder!.becomeFirstResponder()
+    }
+}
+
+
+extension UIView {
+    var      zlayer:               CALayer { get { return layer } }
+    var recognizers: [ZGestureRecognizer]? { get { return gestureRecognizers } }
+
+
+    func clear() { zlayer.isOpaque = false }
+    func display() {}
+
+
+    @discardableResult func createGestureRecognizer(_ target: ZGestureRecognizerDelegate, action: Selector?, clicksRequired: Int) -> ZGestureRecognizer {
+        let              gesture = UITapGestureRecognizer(target: target, action: action)
+        isUserInteractionEnabled = true
+
+        if recognizers != nil {
+            clearGestures()
+        }
+
+        addGestureRecognizer(gesture)
+
+        return gesture
+    }
+}
+
+
+extension UISegmentedControl {
+    var selectedSegment: Int { get { return selectedSegmentIndex } }
+}
+
+
+extension UITextField {
+    var isBordered : Bool { get { return borderStyle != .none } set { borderStyle = (newValue ? .line : .none) } }
+    func abortEditing() {}
+}
+
+
+public extension UISlider {
+    var doubleValue: Double {
+        get { return Double(value) }
+        set { value = Float(newValue) }
+    }
+}
+
+
+extension UIButton {
+    @objc func nuttin() {}
+
+
+    var onHit: Selector { get { return #selector(nuttin) } set { } }
+
+
+    var title: String? {
+        get { return title(for: .normal) }
+        set { setTitle(newValue, for: .normal) }
+    }
+
+    var isCircular: Bool {
+        get { return true }
+        set { }
+    }
+}
---
+++ Zones/Zones/main/ZonesExtensions.swift
@@ -0,0 +1,157 @@
+//
+//  ZonesExtensions.swift
+//  Zones
+//
+//  Created by Jonathan Sand on 1/31/17.
+//  Copyright © 2017 Zones. All rights reserved.
+//
+
+
+import Foundation
+import Cocoa
+
+
+public typealias ZFont                      = NSFont
+public typealias ZView                      = NSView
+public typealias ZEvent                     = NSEvent
+public typealias ZImage                     = NSImage
+public typealias ZColor                     = NSColor
+public typealias ZButton                    = NSButton
+public typealias ZSlider                    = NSSlider
+public typealias ZWindow                    = NSWindow
+public typealias ZControl                   = NSControl
+public typealias ZMenuItem                  = NSMenuItem
+public typealias ZTextView                  = NSTextView
+public typealias ZTextField                 = NSTextField
+public typealias ZTableView                 = NSTableView
+public typealias ZStackView                 = NSStackView
+public typealias ZButtonCell                = NSButtonCell
+public typealias ZBezierPath                = NSBezierPath
+public typealias ZSearchField               = NSSearchField
+public typealias ZApplication               = NSApplication
+public typealias ZViewController            = NSViewController
+public typealias ZSegmentedControl          = NSSegmentedControl
+public typealias ZGestureRecognizer         = NSGestureRecognizer
+public typealias ZProgressIndicator         = NSProgressIndicator
+public typealias ZTextFieldDelegate         = NSTextFieldDelegate
+public typealias ZTableViewDelegate         = NSTableViewDelegate
+public typealias ZTableViewDataSource       = NSTableViewDataSource
+public typealias ZSearchFieldDelegate       = NSSearchFieldDelegate
+public typealias ZApplicationDelegate       = NSApplicationDelegate
+public typealias ZGestureRecognizerDelegate = NSGestureRecognizerDelegate
+
+
+let zapplication = NSApplication.shared()
+
+
+func CGSizeFromString(_ string: String) -> CGSize {
+    let size = NSSizeFromString(string)
+
+    return CGSize(width: size.width, height: size.height)
+}
+
+
+extension NSApplication {
+    func clearBadge() {
+        dockTile.badgeLabel = ""
+    }
+}
+
+
+extension NSColor {
+    func darker(by: CGFloat) -> NSColor {
+        return NSColor(calibratedHue: hueComponent, saturation: saturationComponent * 1.1, brightness: brightnessComponent / by, alpha: alphaComponent)
+    }
+}
+
+
+
+extension NSView {
+    var      zlayer:                CALayer { get { wantsLayer = true; return layer! } set { layer = newValue } }
+    var recognizers: [NSGestureRecognizer]? { get { return gestureRecognizers } }
+
+
+    func clear() {
+        zlayer.backgroundColor = ZColor.clear.cgColor
+    }
+
+
+    @discardableResult func createGestureRecognizer(_ target: ZGestureRecognizerDelegate, action: Selector?, clicksRequired: Int) -> NSGestureRecognizer {
+        let                            gesture = NSClickGestureRecognizer(target: target, action: action)
+        gesture.numberOfClicksRequired         = clicksRequired
+        gesture.delaysPrimaryMouseButtonEvents = false
+        gesture.delegate                       = target
+
+        addGestureRecognizer(gesture)
+
+        return gesture
+    }
+}
+
+
+extension NSButton {
+    var isCircular: Bool {
+        get { return true }
+        set { bezelStyle = newValue ? .circular : .rounded }
+    }
+
+    var onHit: Selector? {
+        get { return action }
+        set { action = newValue; target = self } }
+}
+
+
+extension NSTextField {
+    var textAlignment : NSTextAlignment { get { return alignment } set { alignment = newValue } }
+    var text: String? {
+        get { return stringValue }
+        set { stringValue = newValue! }
+    }
+}
+
+
+extension NSSegmentedControl {
+    var selectedSegmentIndex: Int {
+        get { return selectedSegment }
+        set { selectedSegment = newValue }
+    }
+}
+
+
+extension NSProgressIndicator {
+    func startAnimating() { startAnimation(self) }
+    func  stopAnimating() {  stopAnimation(self) }
+}
+
+
+public extension NSImage {
+    public func imageRotatedByDegrees(_ degrees: CGFloat) -> NSImage {
+
+        var imageBounds = NSZeroRect ; imageBounds.size = self.size
+        let pathBounds = NSBezierPath(rect: imageBounds)
+        var transform = NSAffineTransform()
+        transform.rotate(byDegrees: degrees)
+        pathBounds.transform(using: transform as AffineTransform)
+        let rotatedBounds:CGRect = NSMakeRect(NSZeroPoint.x, NSZeroPoint.y , self.size.width, self.size.height )
+        let rotatedImage = NSImage(size: rotatedBounds.size)
+
+        //Center the image within the rotated bounds
+        imageBounds.origin.x = NSMidX(rotatedBounds) - (NSWidth(imageBounds) / 2)
+        imageBounds.origin.y  = NSMidY(rotatedBounds) - (NSHeight(imageBounds) / 2)
+
+        // Start a new transform
+        transform = NSAffineTransform()
+        // Move coordinate system to the center (since we want to rotate around the center)
+        transform.translateX(by: +(NSWidth(rotatedBounds) / 2 ), yBy: +(NSHeight(rotatedBounds) / 2))
+        transform.rotate(byDegrees: degrees)
+        // Move the coordinate system bak to normal
+        transform.translateX(by: -(NSWidth(rotatedBounds) / 2 ), yBy: -(NSHeight(rotatedBounds) / 2))
+        // Draw the original image, rotated, into the new image
+        rotatedImage.lockFocus()
+        transform.concat()
+        self.draw(in: imageBounds, from: NSZeroRect, operation: .copy, fraction: 1.0)
+        rotatedImage.unlockFocus()
+
+        return rotatedImage
+    }
+}
