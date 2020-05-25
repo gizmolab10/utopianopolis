9079a62fb61cb6f8f05dda242aaaad3703c24799
jonathan sand <ideas@civilization2.org>
2020-05-25 16:44:16 -0700

    snapkit upgrade

---
+++ Carthage snapkit 1.0/Checkouts/SnapKit/SnapKitFatty/SnapKitFatty.h
@@ -0,0 +1,19 @@
+//
+//  SnapKitFatty.h
+//  SnapKitFatty
+//
+//  Created by Jonathan Sand on 4/4/19.
+//  Copyright © 2019 SnapKit Team. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SnapKitFatty.
+FOUNDATION_EXPORT double SnapKitFattyVersionNumber;
+
+//! Project version string for SnapKitFatty.
+FOUNDATION_EXPORT const unsigned char SnapKitFattyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SnapKitFatty/PublicHeader.h>
+
+
